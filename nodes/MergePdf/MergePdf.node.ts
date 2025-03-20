fbf5d4553ce8d4d883c8c8b8e5d5f2a8132403a1
olaf11071107 <olaf11071107@outlook.com>
2025-03-20 11:10:51 -0400

    Add MergePdf node and associated SVG icon; update package details

---
+++ nodes/MergePdf/MergePdf.node.ts
@@ -0,0 +1,77 @@
+import {
+  IDataObject,
+  IExecuteFunctions,
+  INodeExecutionData,
+  INodeType,
+  INodeTypeDescription,
+  NodePropertyTypes,
+} from "n8n-workflow";
+
+export class MergePdf implements INodeType {
+  description: INodeTypeDescription = {
+    displayName: "Merge PDF",
+    name: "mergePdf",
+    icon: "file:customJs.svg",
+    group: ["transform"],
+    version: 1,
+    description: "Merge PDF",
+    defaults: {
+      name: "Merge PDF",
+    },
+    inputs: ["main"],
+    outputs: ["main"],
+    credentials: [
+      {
+        name: "customJsApi",
+        required: true,
+      },
+    ],
+    properties: [], // Remove the dataFieldName property
+  };
+
+  async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
+    const items = this.getInputData();
+    const returnData: INodeExecutionData[] = [];
+
+    const files = items.map((item, i) => {
+      if (!item.binary?.data) {
+        throw new Error(`No binary data found in "data" field for item ${i}`);
+      }
+      return Buffer.from(item.binary.data.data, "base64").toString("binary");
+    });
+
+    console.log(files);
+
+    const credentials = await this.getCredentials("customJsApi");
+
+    const options = {
+      url: `https://e.customjs.io/__js1-${credentials.apiKey}`,
+      method: "POST",
+      headers: {
+        "customjs-origin": "n8n/mergePDFs",
+        "x-api-key": credentials.apiKey,
+      },
+      body: {
+        input: files,
+        code: "const { PDF_MERGE } = require('./utils'); const pdfBuffers = input.map(obj => Buffer.from(obj).toString('base64')); return PDF_MERGE(pdfBuffers)",
+        returnBinary: "true",
+      },
+      encoding: null,
+      json: true,
+    };
+
+    const response = await this.helpers.request(options);
+
+    returnData.push({
+      json: {} as IDataObject,
+      binary: {
+        data: {
+          data: response,
+          mimeType: "application/pdf",
+        },
+      },
+    });
+
+    return [returnData];
+  }
+}
