ae05d0170985c123dba733213caf0a684f16f593
olaf11071107 <olaf11071107@outlook.com>
2025-04-24 13:02:00 -0400

    Enhance MergePdfs node: add resource options and improve error handling for binary data retrieval

--- nodes/MergePdfs/MergePdfs.node.ts
+++ nodes/MergePdfs/MergePdfs.node.ts
@@ -26,50 +26,93 @@ export class MergePdfs implements INodeType {
         required: true,
       },
     ],
-    properties: [],
+    properties: [
+      {
+        displayName: "Resource",
+        name: "resource",
+        type: "options",
+        options: [
+          {
+            name: "Binary PDF",
+            value: "binary",
+          },
+          {
+            name: "URL",
+            value: "url",
+          },
+        ],
+        default: "binary",
+      },
+      {
+        displayName: "Data field name or URL array (seperate by comma)",
+        name: "field_name",
+        type: "string",
+        default: "data",
+        description:
+          "The field names for binary PDF file or urls that indicate PDF files. Please make sure the size of PDf file doesn't exceed 6mb. If it's bigger, pass an array of URLs rather than binary file.",
+        required: true,
+      },
+    ],
   };
 
   async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
     const items = this.getInputData();
     const returnData: INodeExecutionData[] = [];
 
-    const files = items.map((item, i) => {
-      if (!item.binary?.data) {
-        throw new Error(`No binary data found in "data" field for item ${i}`);
+    const getFile = (field_name: string, i: number) => {
+      const file = items[i].binary?.[field_name];
+      if (!file) {
+        throw new Error(
+          `No binary data found in field "${field_name}" for item ${i}`
+        );
       }
-      return Buffer.from(item.binary.data.data, "base64");
-    });
-
-    const credentials = await this.getCredentials("customJsApi");
-
-    const options = {
-      url: `https://e.customjs.io/__js1-${credentials.apiKey}`,
-      method: "POST",
-      headers: {
-        "customjs-origin": "n8n/mergePDFs",
-        "x-api-key": credentials.apiKey,
-      },
-      body: {
-        input: files,
-        code: "const { PDF_MERGE } = require('./utils'); const pdfBuffers = input.map(base64Str => Buffer.from(base64Str, 'base64')); return PDF_MERGE(pdfBuffers)",
-        returnBinary: "true",
-      },
-      encoding: null,
-      json: true,
+      return Buffer.from(file.data, "base64");
     };
 
-    const response = await this.helpers.request(options);
-    const binaryData = await this.helpers.prepareBinaryData(
-      response,
-      "output.pdf"
-    );
+    for (let i = 0; i < items.length; i++) {
+      const credentials = await this.getCredentials("customJsApi");
+      const isBinary =
+        (this.getNodeParameter("resource", i) as string) === "binary";
+      const field_name = this.getNodeParameter("field_name", i) as string;
 
-    returnData.push({
-      json: {} as IDataObject,
-      binary: {
-        data: binaryData,
-      },
-    });
+      const files = isBinary
+        ? field_name.split(",").map((name) => getFile(name, i))
+        : [];
+      const urls = !isBinary ? field_name.split(",") : [];
+
+      const options = {
+        url: `https://e.customjs.io/__js1-${credentials.apiKey}`,
+        method: "POST",
+        headers: {
+          "customjs-origin": "n8n/mergePDFs",
+          "x-api-key": credentials.apiKey,
+        },
+        body: {
+          input: isBinary ? { files } : { urls },
+          code: `
+              const { PDF_MERGE } = require('./utils'); 
+              input = [...input.files || [],...input.urls || []].filter(i => i); 
+              const pdfBuffers = input.map(obj => obj.data ? Buffer.from(obj.data).toString('base64'): obj); 
+              return PDF_MERGE(pdfBuffers);`,
+          returnBinary: "true",
+        },
+        encoding: null,
+        json: true,
+      };
+
+      const response = await this.helpers.request(options);
+      const binaryData = await this.helpers.prepareBinaryData(
+        response,
+        "output.pdf"
+      );
+
+      returnData.push({
+        json: {} as IDataObject,
+        binary: {
+          data: binaryData,
+        },
+      });
+    }
 
     return [returnData];
   }
