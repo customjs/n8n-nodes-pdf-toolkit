3b1f687da701284298fb6b79907194850bcd1b52
Henrik Lippke <info@henriklippke.de>
2025-10-07 16:55:56 +0200

    Add New Modules

---
+++ nodes/Html2Docx/Html2Pdf.node.ts
@@ -0,0 +1,82 @@
+import {
+  IExecuteFunctions,
+  INodeExecutionData,
+  INodeType,
+  INodeTypeDescription,
+} from "n8n-workflow";
+
+export class Html2Pdf implements INodeType {
+  description: INodeTypeDescription = {
+    displayName: "HTML to Docx (Word) (customJs)",
+    name: "html2Docx",
+    icon: "file:customJs.svg",
+    group: ["transform"],
+    version: 1,
+    description: "Convert HTML to Docx (Word)",
+    defaults: {
+      name: "HTML to Docx",
+    },
+    inputs: ["main"],
+    outputs: ["main"],
+    credentials: [
+      {
+        name: "customJsApi",
+        required: true,
+      },
+    ],
+    properties: [
+      {
+        displayName: "HTML Input",
+        name: "htmlInput",
+        type: "string",
+        typeOptions: {
+          rows: 10,
+        },
+        default: "",
+        description: "The HTML content to convert to Docx",
+        required: true,
+      },
+    ],
+  };
+
+  async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
+    const items = this.getInputData();
+    const returnData: INodeExecutionData[] = [];
+
+    for (let i = 0; i < items.length; i++) {
+      const credentials = await this.getCredentials("customJsApi");
+      const htmlInput = this.getNodeParameter("htmlInput", i) as string;
+
+      const options = {
+        url: `https://e.customjs.io/__js1-${credentials.apiKey}`,
+        method: "POST",
+        headers: {
+          "customjs-origin": "n8n/html2Docx",
+          "x-api-key": credentials.apiKey,
+        },
+        body: {
+          input: htmlInput,
+          code: "const { HTML2DOCX } = require('./utils'); return HTML2DOCX(input)",
+          returnBinary: "true",
+        },
+        encoding: null,
+        json: true,
+      };
+
+      const response = await this.helpers.request(options);
+      const binaryData = await this.helpers.prepareBinaryData(
+        response,
+        "output.docx"
+      );
+
+      returnData.push({
+        json: items[i].json,
+        binary: {
+          data: binaryData,
+        },
+      });
+    }
+
+    return [returnData];
+  }
+}
---
+++ nodes/Markdown2Html/Html2Pdf.node.ts
@@ -0,0 +1,77 @@
+import {
+  IExecuteFunctions,
+  INodeExecutionData,
+  INodeType,
+  INodeTypeDescription,
+} from "n8n-workflow";
+
+export class Html2Pdf implements INodeType {
+  description: INodeTypeDescription = {
+    displayName: "Markdown to HTML (customJs)",
+    name: "markdown2html",
+    icon: "file:customJs.svg",
+    group: ["transform"],
+    version: 1,
+    description: "Convert Markdown to HTML",
+    defaults: {
+      name: "Markdown to HTML",
+    },
+    inputs: ["main"],
+    outputs: ["main"],
+    credentials: [
+      {
+        name: "customJsApi",
+        required: true,
+      },
+    ],
+    properties: [
+      {
+        displayName: "Markdown Input",
+        name: "markdownInput",
+        type: "string",
+        typeOptions: {
+          rows: 10,
+        },
+        default: "",
+        description: "The Markdown content to convert to HTML",
+        required: true,
+      },
+    ],
+  };
+
+  async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
+    const items = this.getInputData();
+    const returnData: INodeExecutionData[] = [];
+
+    for (let i = 0; i < items.length; i++) {
+      const credentials = await this.getCredentials("customJsApi");
+      const markdownInput = this.getNodeParameter("markdownInput", i) as string;
+
+      const options = {
+        url: `https://e.customjs.io/__js1-${credentials.apiKey}`,
+        method: "POST",
+        headers: {
+          "customjs-origin": "n8n/markdown2html",
+          "x-api-key": credentials.apiKey,
+        },
+        body: {
+          input: markdownInput,
+          code: "const { MD2HTML } = require('./utils'); return MD2HTML(input)",
+          returnBinary: "false",
+        },
+        encoding: null,
+        json: true,
+      };
+
+      const response = await this.helpers.request(options);
+
+      returnData.push({
+        json: {
+          output: response.toString(),
+        },
+      });
+    }
+
+    return [returnData];
+  }
+}
