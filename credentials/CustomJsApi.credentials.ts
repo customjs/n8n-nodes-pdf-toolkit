c8b2adbe01c9600c2e69ad7bf6035ed5462834f3
Henrik Lippke <info@henriklippke.de>
2025-10-20 22:15:50 +0200

    feat: add authentication and test functionality to CustomJS API credentials

--- credentials/CustomJsApi.credentials.ts
+++ credentials/CustomJsApi.credentials.ts
@@ -1,15 +1,48 @@
-import { ICredentialType, NodePropertyTypes } from "n8n-workflow";
+import {
+  IAuthenticateGeneric,
+  ICredentialTestRequest,
+  ICredentialType,
+  INodeProperties,
+} from "n8n-workflow";
 
 export class CustomJsApi implements ICredentialType {
   name = "customJsApi";
   displayName = "CustomJS API";
-  properties = [
+  documentationUrl = "https://www.customjs.space/";
+  properties: INodeProperties[] = [
     {
       displayName: "API Key",
       name: "apiKey",
-      type: "string" as NodePropertyTypes,
+      type: "string",
+      typeOptions: { password: true },
       default: "",
-      description: "You can get API Key for CustomJS from https://www.customjs.space/"
+      description: "You can get API Key for CustomJS from https://www.customjs.space/",
+      required: true,
     },
   ];
+
+  authenticate: IAuthenticateGeneric = {
+    type: "generic",
+    properties: {
+      headers: {
+        "x-api-key": "={{$credentials.apiKey}}",
+      },
+    },
+  };
+
+  test: ICredentialTestRequest = {
+    request: {
+      baseURL: "https://e.customjs.io",
+      url: "=/__js1-{{$credentials.apiKey}}",
+      method: "POST",
+      headers: {
+        "customjs-origin": "n8n/credential-test",
+      },
+      body: {
+        input: "test",
+        code: "return 'ok'",
+        returnBinary: "false",
+      },
+    },
+  };
 }
